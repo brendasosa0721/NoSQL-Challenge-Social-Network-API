5f2e4a545591f5f4aaa899bbeb37a6eb478958f7
Brenda  Sosa <brenda.sosa@gmail.com>
2022-10-19 20:31:46 -0400

    added code to server file created the routes folder

---
+++ server.js
@@ -0,0 +1,23 @@
+// Creating the server connection
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/NoSQL-Challenge-Social-Network-API', {
+    userFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+mongoose.set('debug' , true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
